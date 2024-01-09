88afabc22c17c0848b4ff8e00dc78d71bd97bc14
Ulug'bek Norsoatov <127186092+Ulugbek-75@users.noreply.github.com>
2024-01-09 23:38:15 +0500

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -353,3 +353,28 @@ class Solution:
             if int(num[i]) % 2 == 1:
                 return num[:i + 1]
         return ""
+# 872
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def get_leaf_values(node, leaves):
+            if not node:
+                return
+
+            if not node.left and not node.right:
+                leaves.append(node.val)
+
+            get_leaf_values(node.left, leaves)
+            get_leaf_values(node.right, leaves)
+
+        leaves1, leaves2 = [], []
+
+        get_leaf_values(root1, leaves1)
+        get_leaf_values(root2, leaves2)
+
+        return leaves1 == leaves2 
