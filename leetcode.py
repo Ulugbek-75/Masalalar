78fadb6b36057fa2489715a022c38c4a048cd2ee
Ulug'bek Norsoatov <127186092+Ulugbek-75@users.noreply.github.com>
2024-01-11 21:49:33 +0500

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -378,3 +378,27 @@ class Solution:
         get_leaf_values(root2, leaves2)
 
         return leaves1 == leaves2 
+
+
+# 1026
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        def helper(node, min_val, max_val):
+            if not node:
+                return max_val - min_val
+
+            min_val = min(min_val, node.val)
+            max_val = max(max_val, node.val)
+
+            left_diff = helper(node.left, min_val, max_val)
+            right_diff = helper(node.right, min_val, max_val)
+
+            return max(left_diff, right_diff)
+
+        return helper(root, root.val, root.val)
